d0f768c386c90f0221655b7990357adfbc5a691d
Austin Law <alaw989@gmail.com>
2025-09-06 17:46:32 -0400

    lowercase a word

--- components/Home/Home.vue
+++ components/Home/Home.vue
@@ -10,7 +10,7 @@
         <div class="relative z-0 overlay w-full h-full bg-black opacity-[.5]"></div>
         <div class="absolute inset-0 flex items-center justify-center z-10 flex-col max-w-[900px] mx-auto px-4 text-center banner-text">
           <h1 class="text-white font-grotesk  text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[5.62rem]">
-            A Symphony of flavors in every dish
+            A symphony of flavors in every dish
           </h1>
           <p class="font-ptsans font-bold text-white max-w-[70%] sm:text-lg md:text-xl lg:text-2xl ">
             We take great pride in crafting unforgettable dishes, and look forward to creating a memorable moment for you.
